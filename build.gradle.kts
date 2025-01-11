ea9e086963d4f0d48e9d5c557fcdf807896382b2
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2025-01-11 12:03:37 -0500

    release 0.0.2-4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,35 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.24"
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
 }
 
 group = "com.razomy"
-version = "0.0.2-3"
+version = "0.0.2-4"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.2.6")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.3.1.1")
+        bundledPlugin("com.intellij.java")
+        pluginVerifier()
+    }
 }
+
+intellijPlatform {
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
+}
+
 
 tasks {
     // Set the JVM compatibility versions
@@ -32,7 +43,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild.set("243.*")
     }
 
     signPlugin {
